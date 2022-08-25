12a1deabe1df44608230ef3e901c6f44b897b077
Eric Ly <eric.ly@arcticwolf.com>
2022-08-25 18:52:03 +0000

    add jwt

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,7 +1,7 @@
 it("should load the page", () => {
   cy.visit("/");
   cy.wait(1000);
-  cy.get('button').click({force: true});
+  cy.get('[data-testid="public-button"]').click({force: true});
   cy.wait(1000);
   cy.get('[data-testid="id"]').should('be.visible').then((id) => {
     expect(id.text().trim()).to.eq('Id: 1');
@@ -9,4 +9,11 @@ it("should load the page", () => {
   cy.get('[data-testid="userId"]').should('be.visible').then((id) => {
     expect(id.text().trim()).to.eq('UserId: 1');
   });
+  cy.wait(1000);
+  cy.get('[data-testid="protected-button"]').click({force: true});
+  cy.wait(1000);
+  cy.get('[data-testid="protected-data"]').should('be.visible').then((id) => {
+    console.log(id);
+    expect(id.text().trim()).to.eq('This is protected');
+  })
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,6 +16,31 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import "@cypress/code-coverage/support";
+import '@testing-library/cypress/add-commands';
+import {data} from '../fixtures/data.json';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+const email = 'eric.ly@arcticwolf.com';
+const scope = 'test-auth0';
+const audience = 'https://api.arcticwolf.com/'
+
+Cypress.Commands.add('loginByAuth0Api', (route = '/') => {
+  const sHeader = JSON.stringify({alg: 'HS256', typ: 'JWT'});
+  const sPayload = JSON.stringify({
+    'https://rtkwlf.io/email': email, // This non-standard claim contains the email address of the user to which the token was issued.
+    iss: 'https://awn-sandbox.auth0.com/', // the issuer of the token
+    sub: email, // the subject to which the token was issued
+    aud: audience, // the audience for which the token is valid
+    iat: KJUR.jws.IntDate.get('now'), // the time at which the token was issued
+    exp: KJUR.jws.IntDate.get('now + 1day'), // the time at which the token expires
+    scope,
+  });
+  const sJWT = KJUR.jws.JWS.sign('HS256', sHeader, sPayload, secret);
+
+  cy.setCookie('awth-token', sJWT);
+  data.token = sJWT
+
+  cy.visit(route);
+})
